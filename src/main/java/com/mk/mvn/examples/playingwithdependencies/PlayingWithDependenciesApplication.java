8683fc5c1f23cba9062b12b43295d0b9010e9b00
Mohit Kanwar <m.m.kanwar@gmail.com>
2020-06-12 23:16:05 +0530

    initial code

---
+++ src/main/java/com/mk/mvn/examples/playingwithdependencies/PlayingWithDependenciesApplication.java
@@ -0,0 +1,13 @@
+package com.mk.mvn.examples.playingwithdependencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayingWithDependenciesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayingWithDependenciesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mk/mvn/examples/playingwithdependencies/PlayingWithDependenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.mvn.examples.playingwithdependencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayingWithDependenciesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
